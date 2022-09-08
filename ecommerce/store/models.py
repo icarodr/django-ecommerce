3f3a23da62e7ab87f37d6af535eae1b6ae93de78
icarodr <it46009@gmail.com>
2022-09-08 15:56:41 -0300

    new model from music api

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Music(models.Model):
+    class Meta:
+        db_table = 'Music'
+    name = models.CharField(max_length=255)
+    style = models.CharField(max_length=50)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
